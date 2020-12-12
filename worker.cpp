ec2596993211b7ca0c10eac3358725a351d43490
J.A. Hasenoot <j.a.hasenoot@umail.leidenuniv.nl>
2020-12-12 08:35:36 -0800

    correct curl implementation for communication between servers

--- worker.cpp
+++ worker.cpp
@@ -5,13 +5,19 @@
 #include <chrono>
 
 int worker_id;
+int master_id;
+int n_workers;
+char** hostnames;
 //using namespace Pistache;
 
 struct HelloHandler : public Pistache::Http::Handler {
   HTTP_PROTOTYPE(HelloHandler);
   void onRequest(const Pistache::Http::Request& request, Pistache::Http::ResponseWriter writer) override{
     if(request.resource().compare("/get_id") == 0 && request.method() == Pistache::Http::Method::Get){
-      writer.send(Pistache::Http::Code::Ok, std::to_string(worker_id)); //
+      writer.send(Pistache::Http::Code::Ok, std::to_string(worker_id)); // return own ID for master election
+    }
+    if(request.resource().compare("/heartbeat") == 0 && request.method() == Pistache::Http::Method::Get){
+      writer.send(Pistache::Http::Code::Ok); // return OK
     }
   }
 };
@@ -22,14 +28,16 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
     return size * nmemb;
 }
 
-bool sendHeartBeat(char* host){
+bool sendHeartBeat(){
   CURL *curl;
   CURLcode res;
   std::string readBuffer;
 
   curl = curl_easy_init();
   if(curl) {
-      curl_easy_setopt(curl, CURLOPT_URL, host);
+      std::string host(hostnames[master_id]);
+      host.append("/heartbeat");
+      curl_easy_setopt(curl, CURLOPT_URL, host.c_str());
       curl_easy_setopt(curl, CURLOPT_PORT, 9080);
       curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
       curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
@@ -43,9 +51,39 @@ bool sendHeartBeat(char* host){
   }
 }
 
-int elect_master(int worker_id, int n_workers, char** hostnames){
-  int lowest_id = worker_id;
+int elect_master(){
+  fprintf(stderr,"Electing new master\n");
 
+  int lowest_id = worker_id;
+  CURL *curl;
+  CURLcode res;
+  std::string readBuffer;
+
+  for(int i = 0; i < n_workers; ++i){
+    if(i != worker_id){
+      curl = curl_easy_init();
+      if(curl) {
+          std::string host(hostnames[i]);
+          host.append("/get_id");
+          curl_easy_setopt(curl, CURLOPT_URL, host.c_str());
+          curl_easy_setopt(curl, CURLOPT_PORT, 9080);
+          curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+          curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+          res = curl_easy_perform(curl);
+          curl_easy_cleanup(curl);
+          
+          if(res == 0){ // we got a response
+            int other_id = atoi(readBuffer.c_str());
+            if(other_id < lowest_id)
+              lowest_id = other_id;
+          }
+      }else{
+        fprintf(stderr,"Could not init curl\n");
+      }
+    }
+  }
+  fprintf(stderr,"new master_id: %d\n",lowest_id);
+  return lowest_id;
 }
 
 int main(int argc, char **argv) {
@@ -57,9 +95,8 @@ int main(int argc, char **argv) {
   }
 
   worker_id = atoi(argv[1]);
-  int master_id;
-  int n_workers = argc - 2;
-  char* hostnames[argc-2];
+  n_workers = argc - 2;
+  hostnames = (char**) malloc(sizeof(char*)*(argc-2));
   for(int i = 0; i < n_workers; ++i){
     hostnames[i] = argv[i+2];
   }
@@ -76,17 +113,17 @@ int main(int argc, char **argv) {
   fprintf(stderr,"Server running at %s:%s\n",addr.host().c_str(),addr.port().toString().c_str());
 
   // INIT master
-  master_id = elect_master(worker_id,n_workers,hostnames);
+  master_id = elect_master();
 
   // INIT clock & heartbeat
-  std::chrono::time_point<std::chrono::system_clock> prev = std::chrono::system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> prev_time = std::chrono::system_clock::now();
   double heartbeat_interval = 5.0;
   int master_down_counter = 0;
 
   while(true){
     // HEARTBEAT
-    if((std::chrono::system_clock::now() - prev).count() > heartbeat_interval){ // every 5 seconds, heartbeat
-      if(!sendHeartBeat(hostnames[master_id])){ // master is down, check every second
+    if((master_id != worker_id) && ((std::chrono::system_clock::now() - prev_time).count() > heartbeat_interval)){ // every 5 seconds, heartbeat
+      if(!sendHeartBeat()){ // master is down, check every second
         master_down_counter++;
         heartbeat_interval = 1.0;
       } else if(master_down_counter > 0){ // if master recovers, go back to 5 seconds heartbeat
@@ -94,13 +131,16 @@ int main(int argc, char **argv) {
         heartbeat_interval = 5.0;
       }
       if(master_down_counter >= 5){ // if master stays down, elect new master
-        master_id = elect_master(worker_id,n_workers,hostnames);
+        master_id = elect_master();
         master_down_counter = 0;
         heartbeat_interval = 5.0;
       }
+      prev_time = std::chrono::system_clock::now();
     }
 
 
 
   }
+
+  free(hostnames);
 }
