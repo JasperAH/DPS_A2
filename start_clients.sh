6d293255a86e7d2ee4bcc38fda43cc07c12b99f8
J.A. Hasenoot <j.a.hasenoot@umail.leidenuniv.nl>
2020-12-16 13:01:23 -0800

    added correct commands to start_clients script

--- start_clients.sh
+++ start_clients.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
-if [ $# -lt 2 ];
+if [ $# -lt 3 ];
 then
-	echo "Provide the number of clients per client-server and the hostnames of the client-servers"
+	echo "Provide the number of clients per client-server, the hostname of a worker/master and the hostnames of the client-servers"
 	exit 1
 fi
 
-experiment=1
 n_clients=$1
-n_messages=0
-n_servers=0
-
-servers=""
-if [ $experiment == 1 ]; then
-	for srv in "${@:1}"
-	do
-		servers="${servers}${servers:+ }$srv"
-	done
-	n_servers=$((n_servers + 1))
-else
-	for srv in "${@:1}"
-	do
-		servers="${servers}${servers:+ }$srv"
-	done
-	n_servers=$((n_servers + 1))
-fi
-
-#ssh user@host "sh -c 'nohup ./my_program > /dev/null 2>&1 &'" TODO mss moeten we nog wat met die dev/null en die 2>&1 &
-
-ids=1
-
-for srv in $servers
+m_hostname=$2
+c_id=0;
+for srv in "${@:2}"
 do
-	if [ $experiment == 1 ]; then
-		ssh $USER@$srv "sh -c 'nohup ./client $n_clients'"
-		ids=$((ids + 1))
-	else
-		ssh $USER@$srv "sh -c 'nohup ./client $n_clients'"
-		ids=$((ids + 1))		
-	fi		
+	ssh $USER@$srv "sh -c 'nohup /home/ddps2008/DPS_A2/client $c_id $m_hostname > /dev/null 2>&1 &'"
+	c_id=$((c_id + 1))
 done
+
+
